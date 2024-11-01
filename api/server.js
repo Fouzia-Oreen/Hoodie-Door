02e9cc9bca7571d8da2148e1c509dd3e8806d093
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-11-01 23:53:14 +0600

    server connected to mongodb

--- api/server.js
+++ api/server.js
@@ -1,5 +1,34 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.require
 const app = express()
-app.listen(3000, ()=> {
-     console.log('app is listening on port: 3000');
+const PORT = process.env.PORT;
+const Mongodb = process.env.MONGO_URI
+
+mongoose.connect(Mongodb)
+.then(() => ("Mongodb connected"))
+.catch((error) => console.log(error))
+
+app.use(cookieParser());
+app.use(express.json())
+app.use(
+     cors({
+          origin : "http://localhost:5173/",
+          methods : ['GET', 'POST', 'PUT', 'DELETE'],
+          allowedHeaders : [
+               "Content-Type",
+               "Authorization",
+               "Cache-Control",
+               "Expires",
+               "Pragma"
+          ],
+          credentials : true
+    })
+)
+
+app.listen(PORT, () => {
+     console.log(`Server is listening on port: ${PORT}`);
 })

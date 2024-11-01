42d80370318d282f61749b4a7eada0aa3deb707c
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-11-02 00:05:13 +0600

    router-provider created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import { BrowserRouter, RouterProvider } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <RouterProvider store={store}>
+      <App />
+    </RouterProvider>
+  </BrowserRouter>,
 )

58c93f7468b761eac693ddda649ad4d4e69b2afd
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-11-05 23:59:47 +0600

    Home section added

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+
+import Banner from './Banner'
+import Catagory from './Catagory'
+import Featured from './Featured'
+
+const Home = () => {
+  return (
+    <>
+     <Banner /> 
+     <Catagory />
+     <Featured />
+    </>
+  )
+}
+
+export default Home

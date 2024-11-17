e26567db8891c1e8229ce1b38ad924b29d79556d
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-11-17 23:41:26 +0600

    admin profile ui added

--- admin/src/pages/Profile.jsx
+++ admin/src/pages/Profile.jsx
@@ -1,9 +1,88 @@
+import { useState } from 'react';
+import profile from '../assets/avatar.png';
+// import axios from 'axios';
+// import { backendUrl } from '../App';
 
 const Profile = () => {
+  const [image, setImage] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [date, setDate] = useState(Number);
+  const [profession, setProfession] = useState('');
+  const [bio, setBio] = useState('');
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      const formData = new FormData()
+      formData.append("image", image)
+      formData.append("name", name)
+      formData.append("email", email)
+      formData.append("date", date)
+      formData.append("profession", profession)
+      formData.append("bio", bio)
+
+      // const response = await axios.post(backendUrl+"/api/product/add", formData)
+      // console.log(response)
+
+    } catch (error) {
+      console.log(error)
+    }
+ }
+
   return (
-    <div>
-      fghfxgjhnzfxhgxdfhg
-    </div>
+
+    <form className="flex flex-col w-full gap-4 " onSubmit={onSubmitHandler}>
+      <label className=' w-full' htmlFor='profile'>
+        <img src={!image ? profile : URL.createObjectURL(image)}  alt="profile" className='rounded-full border-4 border-primary-dark outline-none max-w-[300px] h-[300px] object-contain'/>
+        <input onChange={(e) => setImage(e.target.files[0])}  type="file" id="profile" hidden/>
+      </label>
+      <div className='w-full'>
+        <p className="pl-2 mb-1">Name</p>
+        <input 
+        type="text" 
+        value = {name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name" 
+        className="px-3 py-2 placeholder:text-text-light placeholder:opacity-65 w-full max-w-[500px] "/>
+      </div>
+      <div className='w-full'>
+        <p className="pl-2 mb-1">Email</p>
+        <input 
+        type="email" 
+        placeholder="Enter your email" 
+        value = {email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="px-3 py-2 placeholder:text-text-light placeholder:opacity-65 w-full max-w-[500px] "/>
+      </div>
+      <div className='w-full'>
+        <p className="pl-2 mb-1">Date-Of-Birth</p>
+        <input 
+        type="date" 
+        value = {date}
+        onChange={(e) => setDate(e.target.value)}
+        placeholder="Enter your DOB" 
+        className="px-3 py-2 placeholder:text-text-light placeholder:opacity-65   w-full max-w-[500px] "/>
+      </div>
+      <div className='w-full'>
+        <p className="pl-2 mb-1">Profession</p>
+        <input 
+        type="text" 
+        value = {profession}
+        onChange={(e) => setProfession(e.target.value)}
+        placeholder="Enter your profession" 
+        className="px-3 py-2 placeholder:text-text-light placeholder:opacity-65 w-full max-w-[500px] "/>
+      </div>
+      <div className='w-full'>
+        <p className="pl-2 mb-1">Bio</p>
+        <textarea 
+        type="text" 
+        value = {bio}
+        onChange={(e) => setBio(e.target.value)}
+        placeholder="Enter your bio" 
+        className="px-3 py-2 placeholder:text-text-light placeholder:opacity-65 w-full max-w-[500px] "/>
+      </div>
+    </form>
   )
 }
 
